2fd89f7903837e924f76b5f937596fce5c58c107
Iru Cai <mytbk920423@gmail.com>
2013-10-08 14:02:03 +0800

    Add bbssend
    A C program to implement normal BBS keypresses

---
+++ termtools/bbssend.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <curses.h>
+
+int main(int argc, char *argv[])
+{
+  if (argc!=2){
+    fprintf(stderr, "usage: sendkey <FIFO>\n");
+  }
+  FILE *fp = fopen(argv[1],"w");
+  if (fp == NULL){
+    fprintf(stderr, "Open %s error.\n", argv[1]);
+  }
+  
+  initscr();
+  raw();
+  noecho();
+  keypad(stdscr,TRUE);
+
+  int c;
+  while (1){
+    c = getch();
+    if (c == KEY_F(12)){
+      printw("F12 pressed, press anykey to exit.\n");
+      getch();
+      break;
+    }
+    fprintf(fp, "%c",c);
+	 fflush(fp);
+  }
+
+  endwin();
+  fclose(fp);
+  return 0;
+}
