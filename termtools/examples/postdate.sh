b2da43a9036aba1b432f37d0de2ef81eeca62fb7
Iru Cai <mytbk920423@gmail.com>
2014-01-03 23:26:43 +0800

    Update the example script.

--- termtools/examples/postdate.sh
+++ termtools/examples/postdate.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-post `date -I`
+cd $(dirname $(realpath $0))
+source ../bbsfun
+post `date -I`|tr -d '-'
 echo
-date | iconv -c -t gbk
+# write the article here
+iconv -c -t gbk mytext
+# end of article
 tsave
 
