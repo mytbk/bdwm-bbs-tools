75f86d3a7bfaab10bd78b420f4eeca0d1036ad80
irudog <irudog@alarm>
2014-02-22 11:36:30 +0800

    can now modify smd

--- wwwtools/cbdwm/libbdwm.sh
+++ wwwtools/cbdwm/libbdwm.sh
@@ -9,3 +9,12 @@ bbs_logout(){
     rm -f "/tmp/cookie.$1"
 }
 
+bbs_modsmd(){
+    # usage: bbs_modsmd user [file]
+    # warning: bdwm.net does not support 100-continue, so smd >1024 bytes is not allowed
+    local cookiefile="/tmp/cookie.$1"
+    shift
+    local mysmd="$(iconv -c -t gbk $@)"
+    curl --data-urlencode "plan=${mysmd}" -b "${cookiefile}" "http://www.bdwm.net/bbs/bbsplan2.php"
+}
+
