619feb682494cd78f375c3c22d2377980f7d2b26
Iru Cai <mytbk920423@gmail.com>
2014-06-30 15:36:17 +0800

    Use cookie for queryuser.sh

--- wwwtools/queryuser.sh
+++ wwwtools/queryuser.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+# usage: queryuser.sh [-u <my user id>] <userid>
 
-w3m -dump http://www.bdwm.net/bbs/bbsqry.php?name="$1" | sed '1,4d'
+if [ "$1" == "-u" ]; then
+    shift
+    MYNAME="$1"
+    shift
+fi
+
+USERID="$1"
+
+curl -s -b "/tmp/cookie.$MYNAME" "http://www.bdwm.net/bbs/bbsqry.php?name=$USERID" \
+    | iconv -f GBK \
+    | w3m -dump -T text/html \
+    | sed '1,4d'
 
